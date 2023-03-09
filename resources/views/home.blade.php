3d5c1dc90a52fcfe22cae2312eeeb149e2a584fd
argyaWicaksana <argya7@gmail.com>
2023-03-09 17:24:56 +0700

    install and use Laravel-AdminLTE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,25 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <h1>Hello, this is home</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1>Hello, this is home</h1>
+        </div>
     </div>
-</div>
-@endsection
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
