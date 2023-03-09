1f478d5e4896d5ab87da2154a9b77724981888a9
argyaWicaksana <argya7@gmail.com>
2023-03-09 14:31:54 +0700

    add image and delete some blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,6 @@
 <div class="container">
     <div class="row justify-content-center">
         <h1>Hello, this is home</h1>
-        {{-- <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div> --}}
     </div>
 </div>
 @endsection
